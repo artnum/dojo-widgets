8750cae14bd11a240bd84300ebc513d5665b6b2d
artnum <etienne@artisan-numerique.ch>
2018-06-19 13:35:19 +0200

    standardjs styling

--- Request.js
+++ Request.js
@@ -1,117 +1,131 @@
+/* eslint-env browser, amd */
 define([
-	"dojo/_base/lang",
-	"dojo/Deferred",
-	"dojo/request/xhr"
-], function(
-	djLang,
-	djDeferred,
-	djXhr
-){
-return { 
-		_options: function() {
-			var options =  { handleAs: 'json', cacheTimeout: 240 };
-			if(arguments[0]) {
-				if(arguments[0].query) {
-					options.query = arguments[0].query;	
-				}
-				if(arguments[0].headers) {
-					options.headers = arguments[0].headers;	
-				}
-				if(arguments[0].data) {
-					options.data = arguments[0].data;	
-				}
-				if(arguments[0].cacheTimeout) {
-					options.data = arguments[0].cacheTimeout;	
-				}
-			}
+  'dojo/_base/lang',
+  'dojo/Deferred',
+  'dojo/request/xhr'
+], function (
+  djLang,
+  DjDeferred,
+  djXhr
+) {
+  return {
+    _options: function () {
+      var options = { handleAs: 'json', cacheTimeout: 240 }
+      if (arguments[0]) {
+        if (arguments[0].query) {
+          options.query = arguments[0].query
+        }
+        if (arguments[0].headers) {
+          options.headers = arguments[0].headers
+        }
+        if (arguments[0].data) {
+          options.data = arguments[0].data
+        }
+        if (arguments[0].cacheTimeout) {
+          options.data = arguments[0].cacheTimeout
+        }
+      }
 
-			return options;
-		},
+      return options
+    },
 
-		get: function (url) {
-			var xhr, def = new djDeferred( (reason) => { xhr.cancel(); } );
-			var options = this._options(arguments[1]);
-			options.method = 'GET';
+    get: function (url) {
+      var xhr
+      var def = new DjDeferred((reason) => { xhr.cancel() })
+      var options = this._options(arguments[1])
+      options.method = 'GET'
 
-			xhr = djXhr(url, options);
+      xhr = djXhr(url, options)
       xhr.then(function (r) {
-				def.resolve(r);
-			});
+        def.resolve(r)
+      }, function (r) {
+        def.reject(r)
+      })
 
-			return def.promise;
-		},
+      return def.promise
+    },
 
-		post: function (url) {
-			var xhr, def = new djDeferred( (reason) => { xhr.cancel(); } );
-			var options = this._options(arguments[1]);
-			options.method = 'POST';
+    post: function (url) {
+      var xhr
+      var def = new DjDeferred((reason) => { xhr.cancel() })
+      var options = this._options(arguments[1])
+      options.method = 'POST'
 
-			xhr = djXhr(url, options);
-      xhr.then( function (r) {
-				def.resolve(r);
-			});
+      xhr = djXhr(url, options)
+      xhr.then(function (r) {
+        def.resolve(r)
+      }, function (r) {
+        def.reject(r)
+      })
 
-			return def.promise;			
-		},
+      return def.promise
+    },
 
-		put: function (url) {
-			var xhr, def = new djDeferred( (reason) => { xhr.cancel(); } );
-			var options = this._options(arguments[1]);
-			options.method = 'PUT';
-			
-			xhr = djXhr(url, options);
-      xhr.then( function (r) {
-				def.resolve(r);	
-			});
+    put: function (url) {
+      var xhr
+      var def = new DjDeferred((reason) => { xhr.cancel() })
+      var options = this._options(arguments[1])
+      options.method = 'PUT'
 
-			return def.promise;
-		},
+      xhr = djXhr(url, options)
+      xhr.then(function (r) {
+        def.resolve(r)
+      }, function (r) {
+        def.reject(r)
+      })
 
-		del: function (url) {
-			var xhr, def = new djDeferred( (reason) => { xhr.cancel(); } );
-			var options = this._options(arguments[1]);
-			options.method = 'DELETE';
+      return def.promise
+    },
 
-			xhr = djXhr(url, options);
-      xhr.then( function (r) {
-				def.resolve(r);
-			});
+    del: function (url) {
+      var xhr
+      var def = new DjDeferred((reason) => { xhr.cancel() })
+      var options = this._options(arguments[1])
+      options.method = 'DELETE'
 
-			return def.promise;
-		},
+      xhr = djXhr(url, options)
+      xhr.then(function (r) {
+        def.resolve(r)
+      }, function (r) {
+        def.reject(r)
+      })
 
-		head: function(url) {
-			var xhr = new XMLHttpRequest(), def = new djDeferred((reason) => { xhr.abort(); });
+      return def.promise
+    },
 
-			window.setTimeout( function () {
-				xhr.onreadystatechange = function (event) {
-					if(this.readyState === XMLHttpRequest.DONE) {
-						if(this.status === 200) {
-							var hArtnum = /X-Artnum-([^\:]+)\:([^\r\n]*)/;
-							var hGeneric = /([^\:]+)\:([^\r\n]*)/;
-							var headers = this.getAllResponseHeaders();
-							var result = new Object();
-							headers.split(/[\r\n]/).forEach( function (header) {
-								if(hArtnum.test(header)) {
-									var s = hArtnum.exec(header);
-									result[s[1]] = s[2];
-								} else if(hGeneric.test(header)) {
-									var s = hGeneric.exec(header);
-									result[s[1]] = s[2];
-								}
-							});
-							def.resolve(result);	
-						} else {
-							def.resolve(null);
-						}
-					}
-				}
+    head: function (url) {
+      var xhr = new XMLHttpRequest()
+      var def = new DjDeferred((reason) => { xhr.abort() })
 
-				xhr.open('HEAD', url, true);
-				xhr.send();
-			}, 0);
+      window.setTimeout(function () {
+        xhr.onreadystatechange = function (event) {
+          if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+              var hArtnum = /X-Artnum-([^:]+):([^\r\n]*)/
+              var hGeneric = /([^:]+):([^\r\n]*)/
+              var headers = this.getAllResponseHeaders()
+              var result = {}
+              headers.split(/[\r\n]/).forEach(function (header) {
+                if (hArtnum.test(header)) {
+                  var s = hArtnum.exec(header)
+                  result[s[1]] = s[2]
+                } else if (hGeneric.test(header)) {
+                  s = hGeneric.exec(header)
+                  result[s[1]] = s[2]
+                }
+              })
+              def.resolve(result)
+            } else {
+              def.resolve(null)
+            }
+          }
+        }
 
-			return def.promise;
-		}
-	};
-});
+        xhr.open('HEAD', url, true)
+        xhr.send()
+      }, 0)
+
+      return def.promise
+    }
+  }
+})
